9b7d3a4e9679493acc531a844af80270d41d6a18
Chris Penrose <chris@pleasesendhelp.com>
2014-12-22 19:00:22 +0000

    added stupid FIELD macro to meta.hpp which is a very bad idea im bad

--- include/rtw/meta.hpp
+++ include/rtw/meta.hpp
@@ -6,6 +6,12 @@ namespace rtw
 namespace meta
 {
 
+#define FIELD(Type, Name)\
+	public:\
+		Type Name() const { return Name##_; }\
+	private:\
+		Type Name##_
+
 template <class T, class U> bool is_one_of(const T value, const U & list)
 {
 	for(const auto item : list)
