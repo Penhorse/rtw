60419cec98708b581f449e06a45a133efd811ca0
Chris Penrose <chris@pleasesendhelp.com>
2015-02-04 18:34:36 +0000

    comments

--- include/rtw/meta.hpp
+++ include/rtw/meta.hpp
@@ -6,13 +6,45 @@ namespace rtw
 namespace meta
 {
 
+//
+// FIELD
+//
+// you can use this macro like this if you want:
+//``````````````````````````````````````````````````````````````````````````````
+//	class Fart
+//	{
+//		FIELD(int, butt);
+//	};
+//,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+//
+// it's equivalent to this:
+//``````````````````````````````````````````````````````````````````````````````
+//	class Fart
+//	{
+//	
+//	public:
+//
+//		int butt() const { return butt_; }
+//
+//	private:
+//
+//		int butt_;
+//	
+//	};
+//,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,,
+//
 #define FIELD(Type, Name)\
 	public:\
 		Type Name() const { return Name##_; }\
 	private:\
 		Type Name##_
 
-template <class T, class U> bool is_one_of(const T value, const U & list)
+//
+// returns true if [range] contains [value], otherwise false
+// i dunno why this is in meta.hpp
+// it shouldn't be
+//
+template <class T, class Range> bool is_one_of(const T value, const Range & range)
 {
 	for(const auto item : list)
 	{
