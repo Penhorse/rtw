6ca062424aae7c85bc4477e4e81de56cb05bdc91
Chris Penrose <chris@pleasesendhelp.com>
2015-06-22 00:29:21 +0100

    added is_ready(future)

--- include/rtw/future_util.h
+++ include/rtw/future_util.h
@@ -7,5 +7,11 @@ namespace rtw
 
 template <class Function, class... Args>
 using future_of = std::future<typename std::result_of<Function(Args...)>::type>;
+
+template <class T>
+bool is_ready(const std::future<T> & f)
+{
+	return f.wait_for(std::chrono::seconds(0)) == std::future_status::ready;
+}
 	
 } // namespace rtw
